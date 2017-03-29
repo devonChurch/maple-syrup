ddc2f2ef734bdd318baab987354a41d550614935
strivegfx <notifydevon@gmail.com>
2017-03-29 19:01:30 +1300

    Travis tweaks

--- serverless-deploy.sh
+++ serverless-deploy.sh
@@ -1,10 +1,7 @@
-APPLES="apples =)"
-echo "I like $APPLES"
-
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 echo "AWS key = $AWS_ACCESS_KEY_ID"
 echo "AWS secret = $AWS_SECRET_ACCESS_KEY"
 
-# sls deploy
+sls deploy
