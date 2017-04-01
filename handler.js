98ef464d8714e1b5de12afc9b92607f29d28aca9
strivegfx <notifydevon@gmail.com>
2017-04-02 10:54:50 +1200

    Adding in dev vs prod library src targets

--- handler.js
+++ handler.js
@@ -2,22 +2,39 @@
 
 module.exports.createScaffold = (event, context, callback) => {
 
+	const isProduction = process.env.NODE_ENV === 'production';
+	const createLinkTag = src => `<link rel="stylesheet" href="${src}"/>`;
+	const createScriptTag = src => `<script src="${src}"></script>`;
+	const styles = isProduction ? createLinkTag('/xxxxx/client.css') : '';
+	const script = isProduction ? createScriptTag('/xxxxx/client.js') : createScriptTag('http://localhost:8004/research-hub/client.js');
+	const doctype = '<!doctype html>';
 	const html = (`
+		<html>
 
-        <html>
-            <head>
-                <title>Hello</title>
-            </head>
+			<head>
 
-            <body>
-                <h1>Hello</h1>
+				<meta charset="utf-8">
+				<meta http-equiv="x-ua-compatible" content="ie=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<title>Research Hub</title>
+
+				${styles}
+
+			</head>
+
+			<body>
+
+				<h1>Hello</h1>
 				<p>This content has been generated via an AWS API Gateway request that calls a Lambda to generate the HTML.</p>
 				<p>We can pass in any URL from the a root domain request to an arbitrary nested directory and always return this predictable HTML response.</p>
-            </body>
-        </html>
 
-    `);
+				${script}
 
-	callback(null, html);
+			</body>
+
+		</html>
+	`);
+
+	callback(null, `${doctype}${html}`);
 
 };
